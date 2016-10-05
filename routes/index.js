fce81bc1c5edeab0d5424c9c594fcfc4560d717c
Georgios Psarakis <g.psarakis3800@gmail.com>
2016-10-04 20:33:46 -0400

    Finsihed the JS and ejs pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Georgios Psarakis' });
+});
+router.get('/work', function(req, res, next){
+res.render('work',{title: 'My past Projects'});
+});
+router.get('/skills', function(req,res,next){
+res.render('skills',{title: 'My Skills'});
+});
+router.get('/about', function(req,res,next){
+res.render('about', {title: 'About Me'});
 });
 
 module.exports = router;

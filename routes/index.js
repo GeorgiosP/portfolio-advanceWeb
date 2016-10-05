4af37a31dcadf0771d259a55117c3c7a793cfcdf
Georgios Psarakis <g.psarakis3800@gmail.com>
2016-10-04 21:05:29 -0400

    adding code commenting on what needs to go where

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Georgios Psarakis' });
 });
+/* Get work page */ 
 router.get('/work', function(req, res, next){
 res.render('work',{title: 'My past Projects'});
 });
+/* Get skills page */
 router.get('/skills', function(req,res,next){
 res.render('skills',{title: 'My Skills'});
 });
+/* Get about page */
 router.get('/about', function(req,res,next){
 res.render('about', {title: 'About Me'});
 });
+/* Get Contact page */
 router.get('/contact', function(req,res,next){
 res.render('contact',{title: 'Contact Me'})
 })

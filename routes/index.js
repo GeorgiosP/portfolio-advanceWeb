c3e58e0ae45ab85cd2e4ccfce72cb1b9096d6f24
Georgios Psarakis <g.psarakis3800@gmail.com>
2016-10-11 23:29:07 -0400

    Added working contact page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Georgios Psarakis' });
 });
-/* Get work page */ 
+/* Get work page */
 router.get('/work', function(req, res, next){
 res.render('work',{title: 'My past Projects'});
 });
@@ -22,4 +25,32 @@ router.get('/contact', function(req,res,next){
 res.render('contact',{title: 'Contact Me'})
 })
 
+
+
+var transporter =
+nodemailer.createTransport('smtps://geo.psarakis1234%40gmail.com:baa12345@smtp.gmail.com');
+
+// nodemailer
+router.post('/send', function(req,res){
+  console.log(req.body)
+
+var mailOptions = {
+  from:'"Georgios Psarakis" <geo.psarakis1234@gmail.com>',
+  to: req.body.email,
+  subject: 'Thanks!',
+  text: 'Thanks for wanting to stay in contact with us',
+  html: '<b>Thanks for wanting to stay in contact with us</b>',
+};
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }else{
+res.redirect('/contact')
+      console.log('Message sent: ' + info.response)
+
+}
+
+}); // end of transpeter.sendMail
+
+});// end of router.post
 module.exports = router;
